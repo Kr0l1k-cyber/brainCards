a20ce5b51e569f87a77affaf4c70ec04e57bdce3
Iyrii Pyshnov <coolspot64rus@gmail.com>
2023-04-19 19:16:10 +0300

    crateHeader

---
+++ js/script.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const initApp = () => {
+    const headerParent = document.querySelector('.header');    
+    const app = document.querySelector('#app');
+
+    createHeader(headerParent);
+    
+};
+
+
+initApp();
