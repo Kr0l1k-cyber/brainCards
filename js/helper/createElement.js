b5f4e0b0bd6eee040be767b9d9ec0559e75eb47b
Iyrii Pyshnov <coolspot64rus@gmail.com>
2023-04-19 19:45:16 +0300

    функция помощи создания элемента

---
+++ js/helper/createElement.js
@@ -0,0 +1,5 @@
+'use strict';
+export const createElement = (tag, attr) => {
+    const element =document.createElement(tag);
+    return Object.assign(element, attr)
+}
--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
 'use strict';
+import { createHeader } from "./components/createHeader.Js";
 
 const initApp = () => {
     const headerParent = document.querySelector('.header');    
     const app = document.querySelector('#app');
 
     createHeader(headerParent);
-    
+
 };
 
 
